b61d1453af865cbe4bd99e7b2669664339ce58ab
HOWARD LARRISON <development@daybreak.coffee>
2018-06-16 17:12:13 +0700

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -1,7 +1,7 @@
 from p2pool.bitcoin import networks
 from p2pool.util import math
 
-# CHAIN_LENGTH = number of shares back client keeps
+# CHAIN_LENGTH = number of shares back client keeps.
 # REAL_CHAIN_LENGTH = maximum number of shares back client uses to compute payout
 # REAL_CHAIN_LENGTH must always be <= CHAIN_LENGTH
 # REAL_CHAIN_LENGTH must be changed in sync with all other clients
